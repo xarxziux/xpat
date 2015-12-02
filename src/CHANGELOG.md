fb9565ef7aff177f1c629899e0065185571d1168
xarxziux <hairy.one@virgin.net>
2015-12-02 08:36:45 +0000

    Very basic engine-matching capability added

--- src/CHANGELOG.md
+++ src/CHANGELOG.md
@@ -1,33 +1,62 @@
 # Change log
+###[unreleased]
+###Changed
+ - is_book_position function to accepts a node instead of a board.  Reverted
+   back to original setting - I can't ses how to send the starting node instead
+   of a board to this function for opening book validation.
+   
+ - hard-coding of mate score to a settable value.
+   
+ - variable names for all settings to uppercase.
+   
+ ###Added
+ - basic move-matching capability.  Largely untested and not fully implemented
+   yet but it appears to be working.
+   
+ - evaluate_position function to de-clutter main() loop.
+   
+ - open_book function to handle "file not found" exceptions and perform basic
+   book validation.
+   
+###Removed
+ - running_board variable from main() loop.  Function and function
+   calls changed to accomodate this.
+ 
+###[0.0.2] 2015-12-01
+### v0.0.2 released - exclusion of testing/ and external/ directories
+   successful.
+
 
 ###[unreleased] 2015-12-01
 ### Added
- - Engine and search depth info added to end of game
+ - engine and search depth info to end of game.
    
- - Polyglot book functionality added
+ - Polyglot book functionality.
 
 ### Changed
- - Placed pgn output inside a "with" block instead of keeping it open all the time
+ - Placed pgn output inside a "with" block instead of keeping it open all the
+   time.
    
- - Minor code clean-ups
+ - Minor code clean-ups.
+
 
 ###[unreleased] 2015-11-25
 ### Added
  - .gitattributes to prevent the /testing and /external dircectories from being
-   added to future releases
+   added to future releases.
 
 ### Changed
- - Fixed get_score to limit the return value between -1000 and +1000 
+ - get_score to limit the return value between -1000 and +1000 
    centipawns.
    
-   - Changed the logic of get_score to return an integer value for centipawns 
-   instead of a floating-point value for pawns.  Fixed the pv_score value in main 
-   to reflect this.
+ - the logic of get_score to return an integer value for centipawns.
+   instead of a floating-point value for pawns.  Fixed the pv_score value in
+   main() to reflect this.
    
- -  Sent engine initialisation to separate function.
- 
- # [0.0.1] - 2015-11-25
- - v0.0.1 released
- 
- 
- 
+ - Sent engine initialisation to separate function.
+
+
+###[0.0.1] - 2015-11-25
+### v0.0.1 released.
+
+
--- src/t5_analyser.py
+++ src/t5_analyser.py
@@ -23,17 +23,23 @@ import chess.polyglot
 
 
 #Engine settings
-engine_name = "stockfish6"
-pv_max = 5
-search_depth = 10
-hash_size = 128
-thread_count = 1
+ENGINE_NAME = "stockfish6"
+PV_MAX = 5
+SEARCH_DEPTH = 10
+HASH_SIZE = 128
+THREAD_COUNT = 1
 
 
 #Files
-input_file = "pgn/gamesin.pgn"
-output_file = "pgn/gamesout.pgn"
-book_file = "polyglot/Elo2400.bin"
+INPUT_FILE = "pgn/gamesin.pgn"
+OUTPUT_FILE = "pgn/gamesout.pgn"
+BOOK_FILE = "polyglot/Elo2400.bin"
+#BOOK_FILE = "pgn/gamesout.pgn"
+
+
+#General settings
+MATE_SCORE = 1000
+CP_THRESHOLD = 300
 
 
 def get_score(eng_score, white_to_move):
@@ -43,11 +49,11 @@ def get_score(eng_score, white_to_move):
         
         if (white_to_move):
         
-            white_score = 1000
+            white_score = MATE_SCORE
             
         else:
             
-            white_score = -1000
+            white_score = -MATE_SCORE
             
     else:
         
@@ -59,13 +65,13 @@ def get_score(eng_score, white_to_move):
             
             white_score = (int(-(eng_score.cp)))
             
-        if (white_score > 1000):
+        if (white_score > MATE_SCORE):
             
-            white_score = 1000
+            white_score = MATE_SCORE
             
-        if (white_score < -1000):
+        if (white_score < -MATE_SCORE):
             
-            white_score = -1000
+            white_score = -MATE_SCORE
             
         
     return white_score
@@ -110,34 +116,105 @@ def is_book_position (p_book, c_board):
         
 
 
+def evaluate_position (node, eng):
+    
+    eng.position(node.board())
+    eng.go(depth=SEARCH_DEPTH)
+
+
+def open_book ():
+    
+    try:
+        
+        new_book = chess.polyglot.open_reader (BOOK_FILE)
+        
+    except FileNotFoundError:
+        
+        return False
+        
+    #Validate book
+    if is_book_position (new_book, chess.Board()):
+        
+        return new_book
+        
+    else:
+        
+        return False
+        
+
+
+def find_match (var_list, match_list):
+    
+    match_list[0] += 1
+    i = 1
+    
+    for var in var_list[1:]:
+        
+        if (var_list[0].move == var_list[i].move):
+            
+            match_list[i] += 1
+            return
+            
+        i += 1
+        
+
+
+def init_match_list ():
+    
+    match_list = [0]
+    
+    i = 0
+    
+    while (i < PV_MAX):
+        
+        match_list.append (0)
+        i += 1
+        
+    return match_list
+
+
 def main():
     
     #Engine set-up
-    user_eng_settings = {'name' : engine_name,
-                         'hash' : hash_size,
-                         'multipv' : pv_max,
-                         'threads' : thread_count
+    user_eng_settings = {'name' : ENGINE_NAME,
+                         'hash' : HASH_SIZE,
+                         'multipv' : PV_MAX,
+                         'threads' : THREAD_COUNT
                         }
     engine = init_engine (user_eng_settings)
     engine_data = chess.uci.InfoHandler()
     engine.info_handlers.append(engine_data)
     
-    #Book set-up
-    polyglot_book = chess.polyglot.open_reader (book_file)
+    score_card = init_match_list()
+    #white_match = init_match_list()
+    #black_match = init_match_list()
+    #print (score_card)
     
-    pgn_in = open(input_file, "r")
+    #Book set-up
+    #polyglot_book = chess.polyglot.open_reader (BOOK_FILE)
+    polyglot_book = open_book ()
+    if not polyglot_book:
+        
+        #open_book returned False
+        print ("Bad file name or format for BOOK_FILE.  Exiting.")
+        return
+    
+    pgn_in = open(INPUT_FILE, "r")
     next_game = chess.pgn.read_game(pgn_in)
     
     i = 0
     
     while next_game:
         
+        #Skip main() loop for test purposes
+        #break
+        
         print ("Analysing game ", i+1)
         this_node = next_game.end().parent
         
         while this_node:
             
-            running_board = this_node.board()
+            #running_board = this_node.board()
             
             #This line checks if the next move results in a book position
             if is_book_position (polyglot_book, this_node.variation(0).board()):
@@ -146,25 +223,29 @@ def main():
                 break
                 
             
-            engine.position(running_board)
-            engine.go(depth=search_depth)
+            #engine.position(this_node.board())
+            #engine.go(depth=SEARCH_DEPTH)
+            evaluate_position (this_node, engine)
             
             for j in engine_data.info["pv"]:
                 
                 pvmove = engine_data.info["pv"][j][0]
-                pvscore = (get_score (engine_data.info["score"][j], running_board.turn))/100
+                pvscore = (get_score (engine_data.info["score"][j], this_node.board().turn))/100
                 this_node.add_variation (pvmove,starting_comment = str(pvscore))
                 
+            find_match(this_node.variations, score_card)
+            
             print (".", end = "", flush=True)
             this_node = this_node.parent
             
         print()
+        print (score_card)
         print()
         
         next_game.end().comment = ("T5 analysis performed by '" + engine.name + 
-                "' using a search depth of " + str(search_depth) + ".")
+                "' using a search depth of " + str(SEARCH_DEPTH) + ".")
         
-        with open (output_file, 'a') as pgn_out:
+        with open (OUTPUT_FILE, 'a') as pgn_out:
             
             pgn_out.write(str(next_game))
             pgn_out.write("\n\n")
